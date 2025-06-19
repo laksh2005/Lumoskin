bdf047576b92b9537a8808e2220bf5eea445349b
Laksh Nijhawan <86841869+laksh2005@users.noreply.github.com>
2025-06-19 17:54:22 +0530

    4

--- src/components/BigAd.jsx
+++ src/components/BigAd.jsx
@@ -1,4 +1,4 @@
-import React from "React"
+import React from 'react'
 import bigAdImage from '../assets/7.png';
 
 const BigAd = () => {
--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,28 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
+      external: ['react', 'react-dom'],
       output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
         manualChunks: {
           'react-vendor': ['react', 'react-dom'],
         }
       }
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
+      requireReturnsDefault: 'auto'
     }
   },
   optimizeDeps: {
     include: ['react', 'react-dom']
+  },
+  resolve: {
+    alias: {
+      react: 'react'
+    }
   }
 })
